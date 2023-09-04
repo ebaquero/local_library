789a180b6b7a1a8bec68df4a698f5c33db4ac755
Elias Baquero <141968724+ebaquero@users.noreply.github.com>
2023-09-03 18:59:30 -0700

    Update books.js

--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,30 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  return authors.find(author => author.id === id);
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  return books.find(book => book.id === id);
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  const result = [[], []];
+  books.forEach(book => {
+    const isBorrowed = !book.borrows[0].returned;
+    isBorrowed ? result[0].push(book) : result[1].push(book);
+  });
+  return result
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  const borrows = book.borrows;
+  const result = [];
+  for (const borrow of borrows) {
+    const account = accounts.find(account => account.id === borrow.id);
+    const accountWithStatus = { ...account, returned: borrow.returned };
+    result.push(accountWithStatus);
+  }
+  return result.slice(0, 10);
+}
 
 module.exports = {
   findAuthorById,
