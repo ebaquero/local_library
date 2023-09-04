0aa5d858a4a18ec896488912c6448ff85a5da8ce
Elias Baquero <141968724+ebaquero@users.noreply.github.com>
2023-09-03 18:58:31 -0700

    Update accounts.js

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,29 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
+  return accounts.find(account => account.id === id);
+}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {
+  accounts.sort((accountA, accountB) => (accountA.name.last > accountB.name.last ? 1 : -1));
+  return accounts;
+}
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+  return books.reduce((total, book) => {
+    const borrows = book.borrows;
+    const count = borrows.filter(borrow => borrow.id === account.id).length;
+    return total + count;
+  }, 0);
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  return books.filter(book => {
+    const mostRecentBorrow = book.borrows[0];
+    return mostRecentBorrow.id === account.id && !mostRecentBorrow.returned;
+  }).map(book => {
+    const author = authors.find(author => author.id === book.authorId);
+    return { ...book, author};
+  });
+}
 
 module.exports = {
   findAccountById,
